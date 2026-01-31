d3887ded081f1002da6cb8ad03c162f274cf96fc
Rudra-Bhingarde <rudrabhingardedevacc@gmail.com>
2026-01-31 15:48:50 +0530

    question2 solved

---
+++ question2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+bool isprime(int num);
+
+int main(void){
+    int num;
+    cout<<"enter the number: ";
+    cin>>num;
+    bool prime=isprime(num);
+    if(prime)
+        cout<<"the given number is prime.";
+    else
+        cout<<"the given number is not prime.";
+    return 0;
+}
+
+bool isprime(int num){
+    if(num<2){
+        return false;
+    }
+    if(num==2){
+        return true;
+    }
+    for(int i=2;i*i<=num;i++){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
